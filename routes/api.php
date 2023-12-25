25f3367c12e52dae2b92addf44fc20411424aa8f
mrestuab <mrestuab54@gmail.com>
2023-12-26 06:20:01 +0700

    lapor category, subcategory dan slider sudah dilaksanakan

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\SubcategoryController;
+use App\Http\Controllers\SliderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,4 +23,14 @@ Route::group([
     'prefix' => 'auth'
 ], function(){
     Route::post('login', [AuthController::class, 'login'])-> name('login');
+});
+
+Route::group([
+    'middleware' => 'api',
+], function(){
+    Route::resources([
+       'categories' => CategoryController::class,
+       'subcategories' => SubcategoryController::class,
+       'sliders' => SliderController::class
+    ]);
 });
